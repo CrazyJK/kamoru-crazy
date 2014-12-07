a6f19abe5c9346cb9aa4fa02c1423b3caf8b3d15
kamoru-mac <Crazy.4.JK@gmail.com>
2014-12-07 20:55:47 +0900

    remove crazy-singleton
    	move to https://github.com/CrazyJK/house-of-silence

--- crazy-singleton/src/main/java/jk/kamoru/crazy/image/IMAGE.java
+++
@@ -1,7 +0,0 @@
-package jk.kamoru.crazy.image;
-
-import jk.kamoru.crazy.CRAZY;
-
-public interface IMAGE extends CRAZY {
-
-}
--- crazy-singleton/src/main/java/jk/kamoru/crazy/image/domain/PictureType.java
+++
@@ -1,6 +0,0 @@
-package jk.kamoru.crazy.image.domain;
-
-public enum PictureType {
-
-	MASTER, WEB, THUMBNAIL
-}
